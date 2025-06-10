5132bee6adc70c358887609d48f816d25a3b3c48
Engel Ariel Vargas Sánchez <engesanzincodj@gmail.com>
2025-06-10 17:32:09 -0600

    ya envie los models de Cliente y Cuenta Web ahi voy a enviar todos los que faltan

---
+++ Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneStore_Website.Models
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int Cliente_Id { get; set; }
+
+
+        [Required(ErrorMessage = "La Cedula del Cliente es Requerida")]
+        public required String Cedula { get; set; }
+
+
+        [Required(ErrorMessage = "El Nombre Completo del Cliente es Requerido")]
+        public required String Client_Fullname { get; set; }
+
+
+        [Required(ErrorMessage = "El Gmail del Cliente es Requerido")]
+        public required String Gmail { get; set; }
+
+
+        [Required(ErrorMessage = "El Numero de Celular del Cliente es Requerido")]
+        public required String Telephone { get; set; }
+
+
+        [Required(ErrorMessage = "El Estado del Cliente es Requerido")]
+        public required bool Cliente_Estado { get; set; }
+
+
+        public ICollection<Cuenta_Web> Cuenta_Webs { get; set; } = new List<Cuenta_Web>();
+
+    }
+}
---
+++ Models/Cuenta_Web.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneStore_Website.Models
+{
+    public class Cuenta_Web
+    {
+
+        [Key]
+        public int Cuenta_Id { get; set; }
+
+
+        [Required(ErrorMessage = "La Id del Cliente es Requerida")]
+        public required int Client_Id { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "El Nombre de Usuario es Requerido")]
+        public required String Usuario { get; set; }
+
+
+        [Required(ErrorMessage = "El Gmail del Usuario es Requerido")]
+        public required String Gmail { get; set; }
+
+
+        [Required(ErrorMessage = "La Contraseña es Requerida")]
+        public required String Password { get; set; }
+
+
+        [Required(ErrorMessage = "El Estado de la Cuenta es Requerido")]
+        public required bool Estado_Cuenta { get; set; }
+
+
+
+
+
+    }
+}
